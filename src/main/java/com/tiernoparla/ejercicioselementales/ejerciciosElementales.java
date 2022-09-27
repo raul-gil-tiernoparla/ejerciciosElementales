31ce4fce9373063548fd252f74048fea2b25ca7b
raul-gil-tiernoparla <raul.gildesagredomartin@tiernoparla.es>
2022-09-27 20:09:41 +0200

    terminado ejercicio print pares - for por pares

--- src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -1,16 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.tiernoparla.ejercicioselementales;
 
-/**
- *
- * @author Raul
- */
 public class ejerciciosElementales {
     public static void main(String[] args) {
-        System.out.println("hola");
+        
+        // printear nums pares del 1 al 10
+        for(int i = 2; i < 11; i=i+2){
+            System.out.println(i);
+        } // for
+        
+        
+        
+        
     } // main
 } // ejerciciosElementales
