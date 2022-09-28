9e469b9ee117871cf3e9e2e285b0d32f17686064
raul-gil-tiernoparla <raul.gildesagredomartin@tiernoparla.es>
2022-09-28 18:10:00 +0200

    printear 1 al 10 con while e if

--- src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -4,12 +4,24 @@ public class ejerciciosElementales {
 
     public static void main(String[] args) {
         
+        int n = 1;
+        while(n < 11){
+            if(n%2==0){
+                System.out.println(n);
+            } // if             
+            n++;
+        } // while
+        
+        
+        
         // printear nums pares del 1 al 10 V2
         for(int i = 1; i <= 10; i++){
             if(i%2 == 0){
                 System.out.println(i);
             } // if
         } // for
+       
+        
         
         /*
         // printear nums pares del 1 al 10
