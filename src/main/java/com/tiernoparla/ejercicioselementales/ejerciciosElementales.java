e738d3a47a42a9dcfb6ee7e9dab495da3d0ef088
raul-gil-tiernoparla <raul.gildesagredomartin@tiernoparla.es>
2022-09-28 17:50:15 +0200

    printear del 1 al 10 pares v2

--- src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -1,15 +1,21 @@
 package com.tiernoparla.ejercicioselementales;
 
 public class ejerciciosElementales {
+
     public static void main(String[] args) {
         
+        // printear nums pares del 1 al 10 V2
+        for(int i = 1; i <= 10; i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            } // if
+        } // for
+        
+        /*
         // printear nums pares del 1 al 10
         for(int i = 2; i < 11; i=i+2){
             System.out.println(i);
         } // for
-        
-        
-        
-        
+         */
     } // main
 } // ejerciciosElementales
